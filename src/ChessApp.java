1cb40650cde7214845eca933e67140ed9c624d13
Jonathan Ma <jonathanjma03@gmail.com>
2020-06-27 22:53:29 -0700

    initial commit

---
+++ src/ChessApp.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class ChessApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane mainPane = new BorderPane();
+        Pane gamePane = new Pane();
+
+        Label hi = new Label("hello everyone");
+        gamePane.getChildren().addAll(hi);
+
+        mainPane.setCenter(gamePane);
+        Scene scene = new Scene(mainPane, 500, 500);
+        primaryStage.setTitle("Chess App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
