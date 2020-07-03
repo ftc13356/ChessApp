62010dcedaad7c011fa0529514c9c6a953832d9e
awesomeaamod <awesome.aamod@gmail.com>
2020-07-03 15:09:38 -0700

    I have made the King Class and it compiles

---
+++ src/King.java
@@ -0,0 +1,28 @@
+public class King { //extends ChessPiece
+    private boolean isCastled = false;
+    private String currentLoation;
+    private String targetLocation;
+
+    private boolean hasKingMoved(){
+        boolean hasKingMoved = false;
+        //Body
+        return hasKingMoved;
+    }
+    private boolean isCastleLegal(){
+        boolean castleLegal = true;
+        //Body
+        return castleLegal;
+    }
+    public boolean isLegalMove(){
+        boolean legalMove = true;
+        //Body
+        return legalMove;
+    }
+    public String setTargetLocation(){
+        //Body
+        return targetLocation;
+    }
+    public void moveKing(){
+        //Body
+    }
+}
