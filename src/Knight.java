9d6437d0378c469c7b1418c3259bb1424fa08340
Nathan <nathanko2007@gmail.com>
2020-07-03 15:14:15 -0700

    no message

---
+++ src/Knight.java
@@ -0,0 +1,27 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.util.*;
+
+private location = 0;
+private startingPoint = 0;
+
+
+public class Knight {
+
+}
+
+    public void getLocation {
+
+    }
+
+    public void setLocation{
+
+    }
+
+    public void availableMoves{
+
+    }
