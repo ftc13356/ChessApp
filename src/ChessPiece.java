72e068504de3d265c6a37e520ea92263f1966544
nikhil mehta <nikhilsmehta7@gmail.com>
2020-07-04 20:46:16 -0700

    created the abstract class for chesspiece

---
+++ src/ChessPiece.java
@@ -0,0 +1,29 @@
+abstract class ChessPiece{
+
+    abstract void Move();
+// determines what move to make
+    void MoveLeft(){
+//move left
+    }
+    void MoveRight(){
+//move right
+    }
+    void MoveDown(){
+//move down
+    }
+    void MoveForward(){
+//move forward
+    }
+    void MoveDiagonalUpRight(){
+//Move Diagonal Up and Right
+    }
+    void MoveDiagonalUpLeft(){
+//Move Diagonal Up and Left
+    }
+    void MoveDiagonalDownRight(){
+//Move Diagonal Down and Right
+    }
+    void MoveDiagonalDownLeft(){
+//Move Diagonal Down and Left
+    }
+}
