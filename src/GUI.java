228240fcb754b8cf0f0216786961ebb35c7d7a1f
redpandasforlife <aidenjma@gmail.com>
2020-07-05 19:31:05 -0700

    ChessApp GUI - Aiden

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,37 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.util.*;
+
+/**
+ * This is the Graphical User Interface for the ChessApp
+ *
+ * @author Aiden Ma
+ * @version 1.0
+ */
 public class GUI {
+    private Board board;
+
+    public GUI(Board chessBoard) {
+        board = chessBoard;
+    }
+
+    private void drawBoard() {
+        //draw a board that is 8 squares by 8 squares
+        //alternating between black and white squares
+        //draw numbers(1-8) on vertical
+        //draw letters(a-h) on horizontal
+        //call Player1.draw(g2?); (@sai, write function)
+        //call Player2.draw(g2?); (@sai, write function)
+    }
+
+    public void draw() {
+        drawBoard();
+        //Player1.draw(g2);
+        //Player2.draw(g2);
+    }
 
 }
