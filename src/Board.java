12ed34e51fda6dfc64169aadea483de5f9315c1b
Andrew <andrewluzhao@gmail.com>
2020-07-05 19:21:19 -0700

    This is the Board class framework

---
+++ src/Board.java
@@ -0,0 +1,26 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import java.util.*;
+
+
+public class Knight {
+
+    private String location;
+    private String startingPoint;
+
+    public void getLocation() {
+
+    }
+
+    public void setLocation() {
+
+    }
+
+    public void availableMoves() {
+
+    }
+}
