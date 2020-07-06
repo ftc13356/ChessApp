085752c98f6d334e36e895954c656e93ba5c9432
SpartanSai05 <spartansai05@gmail.com>
2020-07-05 20:12:27 -0700

    Camelcased funstions and variables

--- src/Human.java
+++ src/Human.java
@@ -1,7 +1,7 @@
 
 public class Human extends Player{
     public Human(boolean sidewhite) {
-        this.sidewhite = sidewhite;
-        this.ishuman = true;
+        this.sideWhite = sidewhite;
+        this.isHuman = true;
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,12 +1,12 @@
 public abstract class Player {
-    public boolean sidewhite;
-    public boolean ishuman;
+    public boolean sideWhite;
+    public boolean isHuman;
 
     public boolean isSidewhite() {
-        return this.sidewhite = true;
+        return this.sideWhite = true;
         // Later have to change to input from Human to decide sides
     }
     public boolean isHumanPlayer() {
-        return this.ishuman = true;
+        return this.isHuman = true;
     }
 }
