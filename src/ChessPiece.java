9377e4e468cc47480a27337138335adc5a6b3d07
nikhil mehta <nikhilsmehta7@gmail.com>
2020-07-06 20:50:14 -0700

    declared move and get legal moves as abstract methods. initial commit of the pawn class as well

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,29 +1,6 @@
-abstract class ChessPiece{
+import java.util.ArrayList;
 
-    abstract void move();
-// determines what move to make
-    void moveLeft(){
-//move left
-    }
-    void moveRight(){
-//move right
-    }
-    void moveDown(){
-//move down
-    }
-    void moveForward(){
-//move forward
-    }
-    void moveDiagonalUpRight(){
-//Move Diagonal Up and Right
-    }
-    void moveDiagonalUpLeft(){
-//Move Diagonal Up and Left
-    }
-    void moveDiagonalDownRight(){
-//Move Diagonal Down and Right
-    }
-    void moveDiagonalDownLeft(){
-//Move Diagonal Down and Left
-    }
+abstract class ChessPiece{
+    abstract ArrayList<Location> getLegalMoves();
+    abstract void move(int x, int y);
 }
