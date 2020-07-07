d800c0fff85d9ada57fd7617ef2f15a101a7324b
Nathan <nathanko2007@gmail.com>
2020-07-06 20:56:15 -0700

    no message

--- src/Knight.java
+++ src/Knight.java
@@ -1,19 +1,21 @@
-abstract class Knight extends ChessPiece{
+import com.sun.jdi.Location;
 
-    private String location;
-    private String startingPoint;
+import java.util.ArrayList;
 
+public abstract class Knight extends ChessPiece{
 
-    public void getLocation() {
+    private ChessApp currentLocation;
 
-    }
+    public ArrayList<Location> getLegalMoves() {
+        ArrayList<Location> moves = new ArrayList<Location>();
+        int[] x_y = this.currentLocation.
+        int[] x_directions = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] y_directions = {1, 2, 2, 1, -1, -2, -2, -1};
+        for (int i = 0; i < 8; i++) {
+            Location location = new Location();
+            moves.add(x_directions[i] + y_directions[i]);
+        }
 
-    public void setLocation() {
-
-    }
-
-    public void availableMoves() {
-        availableMoves getLegalMoves = new availableMoves();
-
+        return Knight.Location.setLocation;
     }
 }
