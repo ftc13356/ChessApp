b9f55ae0e42127dfc873f4a219b8e931f8ef1f6c
Nathan <nathanko2007@gmail.com>
2020-07-06 21:28:02 -0700

    no message

--- src/Knight.java
+++ src/Knight.java
@@ -1,21 +1,20 @@
-import com.sun.jdi.Location;
-
 import java.util.ArrayList;
 
 public abstract class Knight extends ChessPiece{
 
-    private ChessApp currentLocation;
+    private Location currentLocation;
 
     public ArrayList<Location> getLegalMoves() {
         ArrayList<Location> moves = new ArrayList<Location>();
-        int[] x_y = this.currentLocation.
-        int[] x_directions = {-2, -1, 1, 2, 2, 1, -1, -2};
-        int[] y_directions = {1, 2, 2, 1, -1, -2, -2, -1};
+        int[] x_y = this.currentLocation.getLocation(); //takes in current location and gets location
+        int[] x_directions = {-2, -1, 1, 2, 2, 1, -1, -2}; //the x of the chessboard ex: x axis = -2, y axis = 1
+        int[] y_directions = {1, 2, 2, 1, -1, -2, -2, -1}; // same as above except y axis
         for (int i = 0; i < 8; i++) {
             Location location = new Location();
-            moves.add(x_directions[i] + y_directions[i]);
+            location.setLocation(x_y[0] + x_directions[i], x_y[1] + y_directions[i]); // 0 and 0 for both the arrays the first time through the loop
+            moves.add(location);
         }
 
-        return Knight.Location.setLocation;
+        return moves;
     }
 }
