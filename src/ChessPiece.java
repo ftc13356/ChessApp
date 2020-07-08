01f9830e0e57b403232c7c026b47a15c34130b92
nikhil mehta <nikhilsmehta7@gmail.com>
2020-07-08 09:47:41 -0700

    made location a variable as well as isLocationOccupied variable

--- src/ChessPiece.java
+++ src/ChessPiece.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
 
 abstract class ChessPiece{
+    public int Location;
     abstract ArrayList<Location> getLegalMoves();
     abstract void move(int x, int y);
+    public int isLocationOccupied;
 }
