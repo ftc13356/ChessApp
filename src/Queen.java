b9693ae20f7b43fa42b42aa9d3093ac902247fe9
WarZ69 <minecraftplayer6542@gmail.com>
2020-07-12 21:21:23 -0700

    Rook class

--- src/Queen.java
+++ src/Queen.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 
 public class Queen extends ChessPiece{
-        int[] queen_pos = {4, 1};
 
         public int[] Location() {
-            return queen_pos;
+            return getLocation();
         }
         public Queen(int x, int y, Board board){
             super(x,y,board);
@@ -14,6 +13,7 @@ public class Queen extends ChessPiece{
             int[] x_directions = {0, 1, 1, 1, 0, -1, -1, -1};
             int[] y_directions = {1, 1, 0, -1, -1, -1, 0, 1};
             ArrayList<Location> available_points=new ArrayList<Location>();
+            int []queen_pos=getLocation();
             for (int i = 0; i < 8; i++) {
                 int x = 1;
                 while (/*board_isOccupied(queen_pos[0] + x_directions[i] * x, queen_pos[1] + y_directions[i] * x)  == false&&queen_pos[0] + x_directions[i] * x<=8&&queen_pos[1] + y_directions[i]<=8&&queen_pos[0] + x_directions[i] * x>0&&queen_pos[1] + y_directions[i]>0 x*/true) {
@@ -28,8 +28,7 @@ public class Queen extends ChessPiece{
         }
 
         public void move(int x, int y) {
-            queen_pos[0]=x;
-            queen_pos[1]=y;
+            setLocation(x,y);
         }
 
         public boolean check(int x, int y) {
---
+++ src/Rook.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Rook extends ChessPiece{
+
+    public Rook(int x, int y, Board boardRef) {
+        super(x, y, boardRef);
+    }
+    public int[] Location() {
+        return getLocation();
+    }
+    public ArrayList<Location> getLegalMoves() {
+        int[] x_directions = {1, -1, 0, 0};
+        int[] y_directions = {0, 0, 1, -1};
+        ArrayList<Location> available_points=new ArrayList<Location>();
+        int[] rook_pos=getLocation();
+        for (int i = 0; i < 8; i++) {
+            int x = 1;
+            while (/*board_isOccupied(rook_pos[0] + x_directions[i] * x, rook_pos[1] + y_directions[i] * x)  == false&&rook_pos[0] + x_directions[i] * x<=8&&rook_pos[1] + y_directions[i]<=8&&rook_pos[0] + x_directions[i] * x>0&&rook_pos[1] + y_directions[i]>0 x*/true) {
+                ArrayList<Integer> points= new ArrayList<Integer>();
+                Location location= new Location();
+                location.setLocation(rook_pos[0] + x_directions[i] * x,rook_pos[1] + y_directions[i] * x);
+                available_points.add(location);
+                x++;
+            }
+        }
+        return available_points;
+    }
+
+    public void move(int x, int y) {
+        setLocation(x,y);
+    }
+
+    public boolean check(int x, int y) {
+        ArrayList<Location> available_points=new ArrayList<Location>();
+        available_points = getLegalMoves();
+        Location location= new Location();
+        location.setLocation(x,y);
+        for (int i = 0; i < available_points.size(); i++) {
+            if (available_points.get(i)==location){
+                return true;
+            }
+        }
+        return false;
+    }
+}
