8092cd4317623c191f766fbb9198fdae9ac354aa
Naomi <>
2020-07-13 13:59:16 -0700

    enhanced location constructor

--- src/Location.java
+++ src/Location.java
@@ -10,6 +10,12 @@ public class Location {
 
     //constructor with any location
     public Location(int x, int y) {
+        if (x < 0 || x > 7) {
+            throw new IllegalArgumentException("X value is less than 0 or more than 7: " + x);
+        }
+        if (y < 0 || y > 7) {
+            throw new IllegalArgumentException("Y value is less than 0 or more than 7: " + y);
+        }
         horizontal = x;
         vertical = y;
     }
