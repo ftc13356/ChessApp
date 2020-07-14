e8687d28e5cce1f6792e3e063b137ddce876f51f
awesomeaamod <awesome.aamod@gmail.com>
2020-07-13 21:57:21 -0700

    Improved Pawn CLass

--- src/Pawn.java
+++ src/Pawn.java
@@ -18,16 +18,15 @@ public class Pawn extends ChessPiece{
         int[] x_y = this.getLocation();
         int[] x_directions = {1}; //the x of the board
         int[] y_directions = {0}; // same as above except y axis
-        for (int i = 0; i < 8; i++) {
-            int new_x = 1;// commented out for testing x_y[0] + x_directions[i]; // new starting location + the x
-            int new_y = 0;// commented out for testing x_y[1] + y_directions[i]; // new starting location + the y
-            if ((locationOccupied(new_x, new_y)) && (isOutOfBoard(new_x, new_y))) ;
-            {
-                Location location = new Location();
-                location.setLocation(new_x, new_y); // sets new location
-                moves.add(location);
-            }
+        int new_x = x_directions[0]; // new starting location + the x
+        int new_y = y_directions[0]; // new starting location + the y
+        if ((locationOccupied(new_x, new_y)) && (isOutOfBoard(new_x, new_y))) ;
+        {
+            Location location = new Location();
+            location.setLocation(new_x, new_y); // sets new location
+            moves.add(location);
         }
+
         return moves;
     }
 
