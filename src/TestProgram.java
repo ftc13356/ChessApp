30686dbcf908c56c0cf0bdbf5c6e4f226da35ddb
awesomeaamod <awesome.aamod@gmail.com>
2020-07-16 14:31:56 -0700

    This is the TestProgram. To run it, you will need to COMMENT out some code in the Board Class

---
+++ src/TestProgram.java
@@ -0,0 +1,27 @@
+public class TestProgram {
+    public static void main(String args[]){
+        /**Instantiating**/
+
+        Player player1 = null;
+        Player player2 = null;
+        Board board1 = new Board(null, null);
+
+        King king = new King(1,1, board1);
+        Knight knight1 = new Knight(8,1,board1);
+        Pawn pawn1 = new Pawn(1,8, board1);
+        Queen queen1 = new Queen(4,4, board1);
+        Rook rook1 = new Rook(8,8, board1);
+
+        System.out.println(king.getLegalMoves());
+        System.out.println("king");
+        System.out.println(knight1.getLegalMoves());
+        System.out.println("knight ");
+        System.out.println(pawn1.getLegalMoves());
+        System.out.println("pawn");
+        System.out.println(queen1.getLegalMoves());
+        System.out.println("queen finished");
+        System.out.println(rook1.getLegalMoves());
+        System.out.println("rook finished");
+
+    }
+}
