bde7cdd6aa81400c569e6fcdd94ca3c72dbff84c
Victor Ma <victormma@gmail.com>
2020-07-17 23:56:33 -0700

    Added a little self test program for Nathan

--- src/Knight.java
+++ src/Knight.java
@@ -37,4 +37,9 @@ public class Knight extends ChessPiece{
     public void move(int x, int y) {
 
     }
+
+    public static void main(String[] args){
+        Knight k = new Knight (1, 1,null);
+        System.out.println(k.getLegalMoves());
+    }
 }
