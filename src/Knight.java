f1b5a129987a2270cf0d3d90db7d16465b635c32
Nathan <nathanko2007@gmail.com>
2020-07-19 21:10:09 -0700

    no message

--- src/Knight.java
+++ src/Knight.java
@@ -5,7 +5,7 @@ public class Knight extends ChessPiece{
     public Knight(int x, int y, Board board) {
         super(x, y, board);
     }
-    
+
 
     public ArrayList<Location> getLegalMoves() {
 
@@ -13,12 +13,13 @@ public class Knight extends ChessPiece{
         int[] x_y = this.getLocation(); //takes in starting point
         int[] x_directions = {-2, -1, 1, 2, 2, 1, -1, -2}; //the x of the chessboard ex: x axis = -2, y axis = 1
         int[] y_directions = {1, 2, 2, 1, -1, -2, -2, -1}; // same as above except y axis
+        Board board = new Board;
 
             for (int i = 0; i < 8; i++) {
                 int new_x = x_y[0] + x_directions[i]; // new starting location + the x
                 int new_y = x_y[1] + y_directions[i]; // new starting location + the y
-                    if ( (!isOutOfBoard(new_x, new_y)) && // ! means not
-                         (locationOccupied(new_x, new_y)) ) {
+                    if ( (!board.isOutOfBoard(new_x, new_y)) && // ! means not
+                         (board.isLocationOccupied(new_x, new_y)) ) {
                         Location location = new Location();
                         location.setLocation(new_x, new_y); // sets location
                         moves.add(location);
