a781a3da3e903d5afd47aa1f2b0b6561958ae1f6
Victor Ma <victormma@gmail.com>
2020-07-23 00:08:56 -0700

    enhanced TestProgram

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -6,11 +6,6 @@ public class TestProgram {
         Player player2 = null;
         Board board1 = new Board(null, null);
 
-        Knight knight1 = new Knight(8,1,board1, player1);
-        Pawn pawn1 = new Pawn(1,8, board1, player1);
-        Queen queen1 = new Queen(4,4, board1, player2);
-        Rook rook1 = new Rook(8,8, board1, player1);
-
         King king = new King(1,1, board1, player1);
         System.out.println("========================================================");
         System.out.println("king was place at 1,1");
@@ -18,12 +13,19 @@ public class TestProgram {
         System.out.println("\tgetLegalMoves: " + king.getLegalMoves());
         System.out.println("\n");
 
+        Knight knight1 = new Knight(8,1,board1, player1);
         System.out.println(knight1.getLegalMoves());
         System.out.println("knight ");
+
+        Pawn pawn1 = new Pawn(1,8, board1, player1);
         System.out.println(pawn1.getLegalMoves());
         System.out.println("pawn");
+
+        Queen queen1 = new Queen(4,4, board1, player2);
         System.out.println(queen1.getLegalMoves());
         System.out.println("queen finished");
+
+        Rook rook1 = new Rook(8,8, board1, player1);
         System.out.println(rook1.getLegalMoves());
         System.out.println("rook finished");
 
