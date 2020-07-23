13063d901cb9c53fea0564f1d924c866fb547fde
Siddharth Arutla <sidarutla@gmail.com>
2020-07-23 00:54:23 -0700

    Fixes the legal move calculation for the pawn

--- src/Pawn.java
+++ src/Pawn.java
@@ -16,11 +16,11 @@ public class Pawn extends ChessPiece{
     public ArrayList<Location> getLegalMoves() {
         ArrayList<Location> moves = new ArrayList<Location>();
         int[] x_y = this.getLocation();
-        int[] x_directions = {1}; //the x of the board
-        int[] y_directions = {0}; // same as above except y axis
-        int new_x = x_directions[0]; // new starting location + the x
-        int new_y = y_directions[0]; // new starting location + the y
-        if ((locationOccupied(new_x, new_y)) && (isOutOfBoard(new_x, new_y))) 
+        int[] x_directions = {0}; //the x of the board
+        int[] y_directions = {1}; // same as above except y axis
+        int new_x = x_directions[0] + x_y[0]; // new starting location + the x
+        int new_y = y_directions[0] + x_y[1]; // new starting location + the y
+        if (!locationOccupied(new_x, new_y) && !isOutOfBoard(new_x, new_y) )
         {
             Location location = new Location();
             location.setLocation(new_x, new_y); // sets new location
