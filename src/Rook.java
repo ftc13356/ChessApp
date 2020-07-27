caf40f03e65cc8f6dc459e2f3c4ecea7056d8df8
WarZ69 <minecraftplayer6542@gmail.com>
2020-07-26 20:19:22 -0700

    Updated rook to use the Board functions

--- src/Rook.java
+++ src/Rook.java
@@ -8,15 +8,6 @@ public class Rook extends ChessPiece{
     public int[] Location() {
         return getLocation();
     }
-    public static boolean locationOccupied(int x, int y){ //empty waiting for andrew
-        return false;
-    }
-    public boolean isOutOfBoard( int x, int y){
-        if(x>8||y>8||x<1||y<1){
-            return true;
-        }
-        return false;
-    }
     public ArrayList<Location> getLegalMoves() {
         int[] x_directions = {1, -1, 0, 0};
         int[] y_directions = {0, 0, 1, -1};
@@ -24,7 +15,7 @@ public class Rook extends ChessPiece{
         int[] rook_pos=getLocation();
         for (int i = 0; i < 4; i++) {
             int x = 1;
-            while (locationOccupied(rook_pos[0] + x_directions[i] * x, rook_pos[1] + y_directions[i] * x)  == false&&!isOutOfBoard(rook_pos[0] + x_directions[i] * x, rook_pos[1] + y_directions[i] * x)) {
+            while (getBoard().isLocationOccupied(rook_pos[0] + x_directions[i] * x, rook_pos[1] + y_directions[i] * x)  == null&&!getBoard().isOutOfBoard(rook_pos[0] + x_directions[i] * x, rook_pos[1] + y_directions[i] * x)) {
                 ArrayList<Integer> points= new ArrayList<Integer>();
                 Location location= new Location();
                 location.setLocation(rook_pos[0] + x_directions[i] * x,rook_pos[1] + y_directions[i] * x);
