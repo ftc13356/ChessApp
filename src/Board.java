782aa789f3f7c39bee73392ff16c307f91852870
WarZ69 <minecraftplayer6542@gmail.com>
2020-07-29 15:29:51 -0700

    Fixed is location occupied added printBoard

--- src/Board.java
+++ src/Board.java
@@ -37,7 +37,7 @@ class Board {
         }
     }
     public void printBoard(){
-        for(int i=1;i<9;i++) {
+        for(int i=8;i>0;i--) {
             System.out.println("_________________________________________________");
             for(int j=1;j<9;j++){
                 System.out.print('|');
