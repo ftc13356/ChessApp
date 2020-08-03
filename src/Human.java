4ef9d09478900846ec469f4c0b85e9192960deb9
redpandasforlife <aidenjma@gmail.com>
2020-08-02 20:00:49 -0700

    fixed problem

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,5 @@
+import javafx.application.Platform;
+
 import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -5,6 +7,7 @@ import java.util.List;
 public class Human extends Player{
     public static List listterms = new ArrayList();
     public static int[] arrayterms = {0,0};
+    String[] ui;
 
     public Human(boolean sideWhite, Board board) {
         super(sideWhite, board);
@@ -38,60 +41,66 @@ public class Human extends Player{
 
     public void movePiece() {
         // Create a prompt and get an input from the human player
-        JTextField startField = new JTextField(5);
-        JTextField endField = new JTextField(5);
-        String start,end;
+//        JTextField startField = new JTextField(5);
+//        JTextField endField = new JTextField(5);
+//        String start,end;
+//
+//        JPanel myPanel = new JPanel();
+//        myPanel.add(new JLabel("Coordinates of the Piece"));
+//        myPanel.add(startField);
+//        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+//        myPanel.add(new JLabel("Where the Piece Should Move To"));
+//        myPanel.add(endField);
+//        while (true) {
+//            int result = JOptionPane.showConfirmDialog(null, myPanel,
+//                    "Please Enter Your Move (ex. a3)", JOptionPane.OK_CANCEL_OPTION);
+//            if (result == JOptionPane.OK_OPTION) {
+//                start = startField.getText();
+//                end = endField.getText();
+//                System.out.println(start);
+//                System.out.println(end);
+//                break;
+//            }
+//            else {
+//                continue;
+//            }
+//        }
 
-        JPanel myPanel = new JPanel();
-        myPanel.add(new JLabel("Coordinates of the Piece"));
-        myPanel.add(startField);
-        myPanel.add(Box.createHorizontalStrut(15)); // a spacer
-        myPanel.add(new JLabel("Where the Piece Should Move To"));
-        myPanel.add(endField);
-        while (true) {
-            int result = JOptionPane.showConfirmDialog(null, myPanel,
-                    "Please Enter Your Move (ex. a3)", JOptionPane.OK_CANCEL_OPTION);
-            if (result == JOptionPane.OK_OPTION) {
-                start = startField.getText();
-                end = endField.getText();
-                System.out.println(start);
-                System.out.println(end);
-                break;
-            }
-            else {
-                continue;
-            }
+        Platform.runLater(()-> ui = gui.getPlayerMove());
+        while (ui == null) {
+            System.out.print("");
         }
-        String letter = start.substring(0,1);
-        String number = start.substring(1);
-        String endletter = end.substring(0,1);
-        String endnumber = end.substring(1);
-
-        //Convert the inputs to [x, y] coordinates
-        int number1 = Integer.parseInt(number);
-        number1 = number1;
-        int letternumber = convertToCoordinates(letter);
-        System.out.println(letternumber);
-        System.out.println(number1);
-        int endnumber2 = Integer.parseInt(endnumber);
-        endnumber2 = endnumber2 ;
-        int endletternumber = convertToCoordinates(endletter);
-        System.out.println(endletternumber);
-        System.out.println(endnumber2);
-        ChessPiece chosenPiece = board1.isLocationOccupied(letternumber, number1);
-        System.out.println(chosenPiece);
-        if (chosenPiece==null){
-            System.out.println("There is no piece at the chosen square. Please input again.");
-            movePiece();
-        }
-        Location chosenMove = new Location(endletternumber, endnumber2);
-        if (chosenPiece.move(endletternumber, endnumber2)) {
-            chosenPiece.setLocation(endletternumber, endnumber2);
-        }
-        else {
-            System.out.println("The move is not legal. Please input again.");
-            movePiece();
-        }
+        System.out.println(ui[0]);
+//        String letter = start.substring(0,1);
+//        String number = start.substring(1);
+//        String endletter = end.substring(0,1);
+//        String endnumber = end.substring(1);
+//
+//        //Convert the inputs to [x, y] coordinates
+//        int number1 = Integer.parseInt(number);
+//        number1 = number1;
+//        int letternumber = convertToCoordinates(letter);
+//        System.out.println(letternumber);
+//        System.out.println(number1);
+//        int endnumber2 = Integer.parseInt(endnumber);
+//        endnumber2 = endnumber2 ;
+//        int endletternumber = convertToCoordinates(endletter);
+//        System.out.println(endletternumber);
+//        System.out.println(endnumber2);
+//        ChessPiece chosenPiece = board1.isLocationOccupied(letternumber, number1);
+//        System.out.println(chosenPiece);
+//        if (chosenPiece==null){
+//            System.out.println("There is no piece at the chosen square. Please input again.");
+//            movePiece();
+//        }
+//        Location chosenMove = new Location(endletternumber, endnumber2);
+//        if (chosenPiece.move(endletternumber, endnumber2)) {
+//            chosenPiece.setLocation(endletternumber, endnumber2);
+//        }
+//        else {
+//            System.out.println("The move is not legal. Please input again.");
+//            movePiece();
+//        }
 
 
 
