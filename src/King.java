d1ec3285929f79534fe684617e3ab3372acfdfc1
awesomeaamod <awesome.aamod@gmail.com>
2020-08-04 17:35:50 -0700

    Added capture in King Class

--- src/King.java
+++ src/King.java
@@ -30,12 +30,18 @@ public class King extends ChessPiece{
         int[] x_y = this.getLocation(); //takes in current location and gets location
         int[] x_directions = {-1, -1, 0, 1, 1, 1, 0, -1}; //the x of the chessboard ex: x axis = -1, y axis = 0
         int[] y_directions = {0, 1, 1, 1, 0, -1, -1, -1}; // same as above except y axis
+        boolean side = getPlayer().isSidewhite();
         for (int i = 0; i < 8; i++) {
             int new_x = x_y[0] + x_directions[i]; // new starting location + the x
             int new_y = x_y[1] + y_directions[i]; // new starting location + the y
-            if ( (getBoard().isLocationOccupied(new_x, new_y)==null) && (!getBoard().isOutOfBoard(new_x, new_y)) ){
+            while ( (getBoard().isLocationOccupied(new_x, new_y)==null) && (!getBoard().isOutOfBoard(new_x, new_y)) ){
                 Location location = new Location();
                 location.setLocation(new_x, new_y); // sets new location
+                moves.add(location);
+            }
+            if(getBoard().isLocationOccupied(new_x, new_y)!=null&&getBoard().isLocationOccupied(new_x, new_y).getPlayer().isSidewhite()!=side){
+                Location location= new Location();
+                location.setLocation(new_x, new_y);
                 moves.add(location);
             }
         }
